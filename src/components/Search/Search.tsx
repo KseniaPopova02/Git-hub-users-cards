206b18855cb4c7184101ad905e9907059695898a
Ksenia <kseniapopova02@gmail.com>
2023-07-25 10:02:22 +0300

    add: finished app

---
+++ src/components/Search/Search.tsx
@@ -0,0 +1,44 @@
+import styles from "./Search.module.scss";
+import { ReactComponent as SearchIcon } from "assets/icon-search.svg";
+import { Button } from "components/Button";
+
+interface SearchProps {
+  hasError: boolean;
+  onSubmit: (text: string) => void;
+}
+
+type FormField = {
+  username: HTMLFormElement;
+};
+
+export const Search = ({ hasError, onSubmit }: SearchProps) => {
+  const handleSubmit = (
+    event: React.FormEvent<HTMLFormElement & FormField>
+  ) => {
+    event.preventDefault();
+    const text = event.currentTarget.username.value;
+    if (text.trim()) {
+      onSubmit(text);
+      event.currentTarget.reset();
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className={styles.search}>
+        <label htmlFor="search" className={styles.label}>
+          <SearchIcon />
+        </label>
+        <input
+          type="text"
+          className={styles.textField}
+          id="search"
+          name="username"
+          placeholder="Search GitHub username..."
+        />
+        {hasError && <div className={styles.error}>No result</div>}
+        <Button>Search</Button>
+      </div>
+    </form>
+  );
+};
